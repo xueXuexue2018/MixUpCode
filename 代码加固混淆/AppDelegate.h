31630c1f5ef1be168ce9f9f06b9d436ce7d953bd
XJL <xjl@XJLdeMacBook-Pro.local>
2018-09-14 10:11:34 +0800

    Initial Commit

---
+++ 代码加固混淆/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  代码加固混淆
+//
+//  Created by XJL on 2018/9/14.
+//  Copyright © 2018年 XJL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
